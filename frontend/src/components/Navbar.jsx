229167d135cefaad2398815d03e2aa1f28b3878b
Dipan Sarkar <alexmithun1005@gmail.com>
2025-04-08 12:23:15 +0530

    dashboard section dilyu collection feature added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,53 +8,55 @@ import { RiMoneyRupeeCircleLine } from "react-icons/ri";
 
 function Navbar() {
   return (
-    <nav className="fixed bottom-0 left-0 w-full flex  justify-around items-center bg-white shadow-lg">
-      <NavLink
-        to="/dashboard"
-        className="flex flex-col items-center text-slate-500"
-      >
-        <IoHomeOutline size="30px" />
-        <p to="/" className="text-xs">
-          Home
-        </p>
-      </NavLink>
+    <div className="max-w-4xl bg-red-700">
+      <nav className="fixed bottom-0 left-0 w-full  flex  justify-around items-center bg-white shadow-lg">
+        <NavLink
+          to="/dashboard"
+          className="flex flex-col items-center text-slate-500"
+        >
+          <IoHomeOutline size="30px" />
+          <p to="/" className="text-xs">
+            Home
+          </p>
+        </NavLink>
 
-      <NavLink
-        to="/members"
-        className="flex flex-col items-center text-slate-500"
-      >
-        <FaPeopleGroup size="30px" />
-        <p to="/" className="text-xs">
-          Members
-        </p>
-      </NavLink>
-      
-      <Link
-        className="flex flex-col items-center bg-[#2C62FF]  p-4 rounded-full "
-        to={"/members/add"}
-      >
-        <FaPlus color="white" size={"25px"} />
-      </Link>
+        <NavLink
+          to="/members"
+          className="flex flex-col items-center text-slate-500"
+        >
+          <FaPeopleGroup size="30px" />
+          <p to="/" className="text-xs">
+            Members
+          </p>
+        </NavLink>
 
-      <NavLink
-        to="/payments"
-        className="flex flex-col items-center text-slate-500"
-      >
-        <RiMoneyRupeeCircleLine size={"30px"} />
-        <p to="/" className="text-xs">
-          Payments
-        </p>
-      </NavLink>
-      <NavLink
-        to="/profile"
-        className="flex flex-col items-center text-slate-500"
-      >
-        <IoPersonOutline size={"30px"} />
-        <p to="/" className="text-xs">
-          Profile
-        </p>
-      </NavLink>
-    </nav>
+        <Link
+          className="flex flex-col items-center bg-[#2C62FF]  p-4 rounded-full "
+          to={"/members/add"}
+        >
+          <FaPlus color="white" size={"25px"} />
+        </Link>
+
+        <NavLink
+          to="/payments"
+          className="flex flex-col items-center text-slate-500"
+        >
+          <RiMoneyRupeeCircleLine size={"30px"} />
+          <p to="/" className="text-xs">
+            Payments
+          </p>
+        </NavLink>
+        <NavLink
+          to="/profile"
+          className="flex flex-col items-center text-slate-500"
+        >
+          <IoPersonOutline size={"30px"} />
+          <p to="/" className="text-xs">
+            Profile
+          </p>
+        </NavLink>
+      </nav>
+    </div>
   );
 }
 
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,31 +1,85 @@
-
 import { Link } from "react-router-dom";
 
 import { FaPeopleGroup } from "react-icons/fa6";
 import { RiMoneyRupeeCircleLine } from "react-icons/ri";
 import { BsPersonArmsUp } from "react-icons/bs";
 import Navbar from "../components/Navbar";
+import { useContext, useEffect, useState } from "react";
+import { paymentContext } from "../context/PaymentContextProvider";
 
 function Dashboard() {
-  // const { trainerData } = useContext(TrainerContext);
-  // console.log(trainerData);
+  const { payments, setPayments } = useContext(paymentContext);
+  const [loading, setLoading] = useState(false);
+  const baseUrl = import.meta.env.VITE_BASE_URL;
+  const d = new Date();
+  const thisMonth = d.getMonth();
+  const monthArr = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let monthName = monthArr[thisMonth];
+  let monthlyCollection;
+  let todayDate = d.getDate();
+  let todayPayments = [];
+  let todayTotalCollection;
 
-  // if (!trainerData)
-  //   return (
-  //     <div className="h-screen w-full bg-green-100 flex flex-col items-center justify-center">
-  //       <h1 className="text-2xl font-medium mb-3 ">OOPS ! No data found !</h1>
-  //       <p className="text-lg capitalize font-medium">
-  //         please{" "}
-  //         <Link className="text-blue-600" to={"/login"}>
-  //           Login
-  //         </Link>{" "}
-  //         again !
-  //       </p>
-  //     </div>
-  //   );
+  // Fetch payments to show the details on dashboard
+  useEffect(() => {
+    async function getPayments() {
+      try {
+        setLoading(true);
+        const url = `${baseUrl}payments`;
+        const response = await fetch(url);
+        const result = await response.json();
+        // set the fetched data to the context
+        setPayments(result.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("An error occurred while fetching payments data");
+        return;
+      }
+    }
+    getPayments();
+  }, []);
+
+  // get total payment collection this month
+
+  if (payments.length) {
+    monthlyCollection = payments.reduce((acc, curr) => {
+      const paymentMonth = new Date(curr.createdAt).getMonth();
+      if (paymentMonth == thisMonth) {
+        return acc + curr.amount;
+      }
+      return acc;
+    }, 0);
+
+    // get total collection of the day
+    todayTotalCollection = payments.reduce(
+      (acc, curr) => {
+        let paymentDate = new Date(curr.createdAt).getDate();
+        if (todayDate == paymentDate) {
+          todayPayments.push(curr);
+          return acc + curr.amount;
+        }
+        return acc;
+      },
+
+      0
+    );
+  }
 
   return (
-    <>
+    <div>
       <div className="bg-[#F7F8F9] h-screen">
         <div className="flex justify-between items-center px-3 pt-6">
           <h1 className="text-4xl font-bold ">Dashboard</h1>
@@ -34,12 +88,17 @@ function Dashboard() {
 
         <div className="p-4 ">
           <div className="monthCollaction bg-[#2C62FF] rounded-xl p-4 ">
-            <p className="monthName block  text-white text-lg mb-2">January</p>
+            <p className="monthName block  text-white text-lg mb-2">
+              {monthName || "No data"}
+            </p>
             <h1 className="text-5xl text-white font-semibold">
-              &#8377;<span>5000</span>
+              &#8377;
+              <span>
+                {loading ? "..." : monthlyCollection || "Data not found"}
+              </span>
             </h1>
             <p className="text-white mt-2">
-              Today's collection : &#8377;<span>500</span>
+              Today's collection : &#8377;<span>{todayTotalCollection}</span>
             </p>
           </div>
         </div>
@@ -49,40 +108,49 @@ function Dashboard() {
             <p className="font-bold uppercase text-slate-500 text-sm">
               Daily Collection
             </p>
-            <Link className="capitalize text-blue-600 font-bold text-base">
+            <Link
+              to={"/payments"}
+              className="capitalize text-blue-600 font-bold text-base"
+            >
               {" "}
               See all
             </Link>
           </div>
           <div className="todays-collection rounded-xl px-3 py-2 bg-white shadow-lg">
-            <div className="collection flex justify-between items-center  mb-3">
-              <img
-                src="https://plus.unsplash.com/premium_photo-1671656349322-41de944d259b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZSUyMGltYWdlfGVufDB8fDB8fHww"
-                alt=""
-                className="imgae w-14  h-14 object-cover rounded-3xl "
-              />
-              <div className="collection-details">
-                <p className="collection-name text-gray-500 text-xs">
-                  Rajkumar
-                </p>
-                <h1 className="collection-amount">&#8377;400</h1>
-              </div>
-              <p className="collection-da">25th jan</p>
-            </div>
-            <div className="collection flex justify-between items-center mb-3 ">
-              <img
-                src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZmlsZSUyMGltYWdlfGVufDB8fDB8fHww"
-                alt=""
-                className="imgae w-14  h-14 object-cover rounded-3xl "
-              />
-              <div className="collection-details">
-                <p className="collection-name text-gray-500 text-xs">
-                  Rajkumar
-                </p>
-                <h1 className="collection-amount">&#8377;400</h1>
-              </div>
-              <p className="collection-da">25th Feb</p>
-            </div>
+            {todayPayments.length ? (
+              todayPayments.map((payment , index) => {
+                return (
+                  <>
+                    <div key={index} className="collection flex justify-between items-center  mb-3">
+                      <img
+                        src={payment.senderId.photo}
+                        alt=""
+                        className="imgae w-14  h-14 object-cover rounded-3xl "
+                      />
+                      <div className="collection-details">
+                        <p className="collection-name text-gray-500 text-xs">
+                          {payment.senderId.fullName}
+                        </p>
+                        <h1 className="collection-amount">
+                          &#8377;{payment.amount}
+                        </h1>
+                      </div>
+                      <p className="collection-da">
+                        {
+                          new Date(payment.createdAt)
+                            .toTimeString()
+                            .split(" ")[0]
+                        }
+                      </p>
+                    </div>
+                  </>
+                );
+              })
+            ) : (
+              <p className="Bold text-gray-700 "> No collection today</p>
+            )}
+
+            
           </div>
         </div>
 
@@ -118,7 +186,7 @@ function Dashboard() {
       </div>
 
       <Navbar />
-    </>
+    </div>
   );
 }
 
